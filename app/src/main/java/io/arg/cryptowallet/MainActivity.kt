ca754006897e140c7fcc8864062f41dc1f566985
Athanasios <athanasiosgerostamoulos@gmail.com>
2021-03-03 16:51:08 +0200

    First commit.

---
+++ app/src/main/java/io/arg/cryptowallet/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.arg.cryptowallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
