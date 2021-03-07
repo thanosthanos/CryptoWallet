ebfb809168340373227eaaeda68513e314ce1576
Athanasios <athanasiosgerostamoulos@gmail.com>
2021-03-07 12:15:01 +0200

    Do n token balance requests

--- app/src/main/java/io/arg/cryptowallet/data/repository/TokenRepositoryImpl.kt
+++ app/src/main/java/io/arg/cryptowallet/data/repository/TokenRepositoryImpl.kt
@@ -5,8 +5,10 @@ import io.arg.cryptowallet.constant.Constants
 import io.arg.cryptowallet.data.resource.Resource
 import io.arg.cryptowallet.exception.NoBalanceFoundException
 import io.arg.cryptowallet.data.server.api.TokensApi
+import io.arg.cryptowallet.data.server.model.ERC20Token
 import io.arg.cryptowallet.data.server.model.ERC20TokenList
 import io.arg.cryptowallet.data.server.model.TokenBalance
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -14,6 +16,7 @@ import io.reactivex.schedulers.Schedulers
 class TokenRepositoryImpl(private val service: TokensApi) : TokenRepository {
 
     private val statusSuccess = "1"
+    private lateinit var filteredList: List<ERC20Token>
 
     override fun getTokenBalance(symbol: String, compositeDisposable: CompositeDisposable): MutableLiveData<Resource<TokenBalance>> {
 
@@ -21,14 +24,16 @@ class TokenRepositoryImpl(private val service: TokensApi) : TokenRepository {
 
         data.value = Resource.Loading()
 
+        filteredList = ArrayList()
         compositeDisposable.add(
                 service.getTokensApi().getTokens()
-                        .map {
-                            tokenList: ERC20TokenList -> tokenList.tokens.first { s -> s.symbol == symbol }.address
+                        .flatMapIterable {
+                            tokenList: ERC20TokenList -> getFilteredList(tokenList = tokenList, symbol = symbol)
                         }
-                        .flatMap { token ->
-                            service.getTokensApi().getTokenBalance(token, Constants.apiKey)
+                        .concatMap {
+                            token -> service.getTokensApi().getTokenBalance(address =  token.address, apikey = Constants.apiKey)
                         }
+                        .toList()
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribeOn(Schedulers.io())
                         .subscribe({response -> onResponse(data, response)}, {t -> onFailure(data, t) })
@@ -37,12 +42,19 @@ class TokenRepositoryImpl(private val service: TokensApi) : TokenRepository {
         return data
     }
 
-    private fun onResponse(data: MutableLiveData<Resource<TokenBalance>>, tokenBalance: TokenBalance) {
-        if(tokenBalance.status == statusSuccess) {
-            data.value = Resource.Success(tokenBalance)
-        } else {
-            data.value = Resource.Failure(NoBalanceFoundException())
-        }
+    private fun getFilteredList(tokenList: ERC20TokenList, symbol: String): List<ERC20Token> {
+        filteredList = tokenList.tokens.filter { s -> s.symbol.contains(symbol) }
+
+        return filteredList
+    }
+
+    private fun onResponse(data: MutableLiveData<Resource<TokenBalance>>, tokenBalance: List<TokenBalance>) {
+        print("")
+//        if(tokenBalance.status == statusSuccess) {
+//            data.value = Resource.Success(tokenBalance)
+//        } else {
+//            data.value = Resource.Failure(NoBalanceFoundException())
+//        }
     }
 
     private fun onFailure(data: MutableLiveData<Resource<TokenBalance>>, t: Throwable) {
---
+++ app/src/main/java/io/arg/cryptowallet/data/server/model/TokenResult.kt
@@ -0,0 +1,3 @@
+package io.arg.cryptowallet.data.server.model
+
+data class TokenResult(val symbol: String, val balance: String)
