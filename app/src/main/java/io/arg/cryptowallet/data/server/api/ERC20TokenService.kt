9acc8e01476084f4770d62c3dd7bfbf12210e256
Athanasios <athanasiosgerostamoulos@gmail.com>
2021-03-08 17:48:39 +0200

    Correct etherscan api call!

--- app/src/main/java/io/arg/cryptowallet/data/server/api/ERC20TokenService.kt
+++ app/src/main/java/io/arg/cryptowallet/data/server/api/ERC20TokenService.kt
@@ -11,7 +11,7 @@ interface ERC20TokenService {
     @GET("https://api.ethplorer.io/getTopTokens?limit=100&apiKey=freekey")
     fun getTokens(): Observable<ERC20TokenList>
 
-    @GET("api?module=account&action=tokenbalance&contractaddress=0xde57844f758a0a6a1910a4787ab2f7121c8978c3&tag=latest")
-    fun getTokenBalance(@Query("address") address: String, @Query("apikey") apikey: String): Observable<TokenBalance>
+    @GET("api?module=account&action=tokenbalance&address=0xde57844f758a0a6a1910a4787ab2f7121c8978c3&tag=latest")
+    fun getTokenBalance(@Query("contractaddress") address: String, @Query("apikey") apikey: String): Observable<TokenBalance>
 
 }
