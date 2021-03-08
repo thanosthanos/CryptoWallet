d5f8574a37c670542ea13ba8121029fbddf91f7c
Athanasios <athanasiosgerostamoulos@gmail.com>
2021-03-08 16:34:03 +0200

    Minor refactor.

--- app/src/main/java/io/arg/cryptowallet/constant/Constants.kt
+++ app/src/main/java/io/arg/cryptowallet/constant/Constants.kt
@@ -1,7 +1,5 @@
 package io.arg.cryptowallet.constant
 
 object Constants {
-//    const val ethereumAccountAddress = "0xde57844f758a0a6a1910a4787ab2f7121c8978c3"
-//    const val apiKey = "E5QFXD7ZYRH7THQM5PIXB8JD4GY38SEJZ4"
     const val debounceTimeout = 500L
 }
